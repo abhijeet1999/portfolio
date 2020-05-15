77ac2154c387121ecb29f305f48b1c5fe4793184
abhijeet1999 <abhijeetcherungottil@gmail.com>
2020-05-15 12:36:56 +0530

    Resolved merge conflict by keeping README.md file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+//import 'package:flutter/services.dart';
+//import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -7,16 +15,59 @@ void main() {
   ));
   
 }
+
+
+
   bool isLargeScreen(BuildContext context) {
     return MediaQuery.of(context).size.width > 800;
   }
-class Home extends StatelessWidget {
+  
+class Home extends StatefulWidget {
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String assetPDFPath="";
+  
+  @override
+  void initState(){
+
+    super.initState();
+    getFileFromAsset("assets/resume.pdf").then((f) {
+      setState ( (){
+        assetPDFPath = f.path;
+        print('***************!!!!!!!!!!!!!!;');
+        print(assetPDFPath);
+
+      } );
+    });
+  }
+  Future <File> getFileFromAsset(String asset) async{
+    try {
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      var dir =await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/resume.pdf");
+      print(file);
+      File assetFile = await file.writeAsBytes(bytes);
+      return assetFile;
+      
+    } catch (e) {
+      throw Exception("error in opening url");
+    }
+
+  }
+
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
+        
          
+        children: <Widget>[
+        
           Padding(
             padding: const EdgeInsets.only(top:100.0),
             child: Text('Abhijeet C',
@@ -61,6 +112,7 @@ class Home extends StatelessWidget {
                 ],
                 ),
                 SizedBox(height:10),
+              
                 Row (
                   children: <Widget>[
                     Icon(Icons.home,size:18 ),
@@ -83,10 +135,14 @@ class Home extends StatelessWidget {
                 ],
                 ),
                 SizedBox(height:10),
-                   Padding(
+              Padding(
               padding: const EdgeInsets.all(8.0),
               child: FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (assetPDFPath != null){
+                    Navigator.push(context,MaterialPageRoute(builder: (context)=>PdfViewPage(path:assetPDFPath)) );
+                  }
+                },
                 color: Colors.blue,
                 child: Text(
                   'RESUME',
@@ -101,7 +157,10 @@ class Home extends StatelessWidget {
                   textAlign: TextAlign.start,
                 ),
               ),
-            )
+            ),
+            
+            
+            
               ],
             ),
           ),
@@ -110,7 +169,85 @@ class Home extends StatelessWidget {
         ],
         
       ),
+     
       
+      
+    );
+  }
+}
+class PdfViewPage extends StatefulWidget {
+  final String path;
+
+  const PdfViewPage({Key key, this.path}) : super(key: key);
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State< PdfViewPage > {
+
+  int _totalPages=0;
+  int _currentPage=0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        PDFView(
+          filePath: widget.path,
+          autoSpacing: true,
+          enableSwipe: true,
+          pageSnap: true,
+          swipeHorizontal: true,
+          onError: (e){
+            print(e);
+          },
+          onRender: (_pages){
+           setState(() {
+              _totalPages=_pages;
+            pdfReady =true;
+             
+           });
+
+
+          },
+          onViewCreated: (PDFViewController vc){
+            _pdfViewController = vc;
+          },
+          onPageChanged: (int page, int total){
+            setState(() {
+              
+            });
+          },
+        ),
+        !pdfReady?Center(child:CircularProgressIndicator(),):Offstage()
+
+
+
+      ],
+
+      ),
+     floatingActionButton: Row(
+       mainAxisAlignment: MainAxisAlignment.end,
+       children: <Widget>[
+         _currentPage>0?FloatingActionButton.extended(
+           backgroundColor: Colors.red,
+           label: Text("Go to ${_currentPage-1}"),
+           onPressed: (){
+             _currentPage -=1;
+             _pdfViewController.setPage(_currentPage);
+           }, 
+           ):Offstage(),
+           _currentPage<_totalPages?FloatingActionButton.extended(
+           backgroundColor: Colors.green,
+           label: Text("Go to ${_currentPage+1}"),
+           onPressed: (){
+             _currentPage +=1;
+             _pdfViewController.setPage(_currentPage);
+           }, 
+           ):Offstage()
+
+     ],), 
     );
   }
 }
